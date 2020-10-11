e23119416570b20f8f958c08767444fec0bd68f8
KreogenGames <70753488+KreogenGames@users.noreply.github.com>
2020-10-11 15:44:33 +0300

    Initial commit

---
+++ QueDouble.java
@@ -0,0 +1,54 @@
+public class QueDouble {
+    private Double[] que;
+    private int nElem1;  // текущее количество элементов в очереди
+    private int front1;
+    private int rear1;
+
+    public QueDouble() {
+        que = new Double[10];
+        rear1 = -1;
+        front1 = 0;
+        nElem1 = 0;
+    }
+
+
+    public void insert(Double elem) {
+        if (rear1 == que.length - 1) {  // циклический перенос
+            rear1 = -1;
+        }
+
+        que[++rear1] = elem;//увеличение Rear и вставка
+        nElem1++;  // увеличение количества элементов в очереди
+    }
+
+    public Double remove() {
+        Double temp1 = que[front1++]; // получаем первый элемент из очереди
+
+        if (front1 == que.length) { // циклический перенос
+            front1 = 0;
+        }
+        nElem1--; // уменьшаем количество элементов в очереди
+        return temp1;
+
+    }
+
+    public Double getFront() {
+        return que[front1];
+    }
+
+    public Double getRear() {
+        return que[rear1];
+    }
+
+    public boolean isFull() {
+        return (nElem1 == que.length - 1);
+    }
+
+    public boolean isEmpty() {
+        return (nElem1 == 0);
+    }
+
+    public int getSize() {
+        return nElem1;
+    }
+}
---
+++ Queue.java
@@ -0,0 +1,53 @@
+public class Queue {
+    private Character[] queue;
+    private int nElem;  // текущее количество элементов в очереди
+    private int front;
+    private int rear;
+
+    public Queue() {
+        queue = new Character[20];
+        rear = -1;
+        front = 0;
+        nElem = 0;
+    }
+
+    public void insert(char elem) {
+        if (rear == queue.length - 1) {  // циклический перенос
+            rear = -1;
+        }
+
+        queue[++rear] = elem;//увеличение Rear и вставка
+        nElem++;  // увеличение количества элементов в очереди
+    }
+
+    public char remove() {
+        char temp = queue[front++]; // получаем первый элемент из очереди
+
+        if (front == queue.length) { // циклический перенос
+            front = 0;
+        }
+        nElem--; // уменьшаем количество элементов в очереди
+        return temp;
+
+    }
+
+    public char getFront() {
+        return queue[front];
+    }
+
+    public char getRear() {
+        return queue[rear];
+    }
+
+    public boolean isFull() {
+        return (nElem == queue.length - 1);
+    }
+
+    public boolean isEmpty() {
+        return (nElem == 0);
+    }
+
+    public int getSize() {
+        return nElem;
+    }
+}
